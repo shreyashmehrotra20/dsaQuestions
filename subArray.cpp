c2350f4e9aacd8ae4c9414bbd8ff91fd1d191c61
Shreyash Mehrotra <shreyashmehrotra20@gmail.com>
2022-04-20 02:10:03 +0530

    chootu change

--- subArray.cpp
+++ subArray.cpp
@@ -1,17 +1,18 @@
 #include <iostream>
 using namespace std;
 int subArray(int arr[], int n)
-{   int max =0 ;
+{   int maximum =0 ;
     for(int i =0;i<n;i++)
     {
       for(int j =i ; j<n;j++)
       {   int sum =0 ;
           for(int k =i ;k<j;k++)
           {
-              cout<<arr[k]<<",";
+             
               sum +=arr[k];
           }
-          cout<<endl;
+         maximum = max(maximum,sum);
+          /* this is the long method we can do the same thig by using max variable
           if(max<=sum)
           {
              max = sum;
@@ -22,13 +23,14 @@ int subArray(int arr[], int n)
               
               continue;
           }
+          */
       }
     }
-    return max;
+    return maximum;
 
 }
 int main() {
-    int arr[] = {10,20,30,40,50,60};
+    int arr[] = {-2,3,4,-1,5,-12,6,1,3};
     int n = sizeof(arr)/sizeof(int);
     int maxSum = subArray(arr,n);
     cout<<"max sum "<< maxSum;
