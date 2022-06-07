3f377f9f841dcbed1585731edc8705abbfaa2e71
Shreyash Mehrotra <shreyashmehrotra20@gmail.com>
2022-06-07 11:30:24 +0530

    selectionSort

---
+++ selectionSort.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+void selectionSort(vector<int> arr)
+{  int n = arr.size();
+   for(int i =0;i<n-1;i++) // i<n-1 because last element will be automatically sorted ,in this loop we are traversing whole array
+   {  
+     
+      int min_position = i;
+      for(int j =i;j<n;j++)
+      {
+           if(arr[j]<arr[min_position])
+           {
+                min_position = j;
+           }
+      }
+      swap(arr[i],arr[min_position]);
+   }
+   for(int i =0 ; i<n;i++)
+   {
+       cout<<arr[i]<<" ";
+   }
+}
+int main() {
+    vector<int> arr = {2,4,1,3,7,5,6};
+    selectionSort(arr);
+    return 0; 
+}
