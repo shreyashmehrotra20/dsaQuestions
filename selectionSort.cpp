2544edb5eb7e6387eda214c92674454c88d6d255
Shreyash Mehrotra <shreyashmehrotra20@gmail.com>
2022-06-07 11:31:06 +0530

    hehe

--- selectionSort.cpp
+++ selectionSort.cpp
@@ -7,14 +7,14 @@ void selectionSort(vector<int> arr)
    {  
      
       int min_position = i;
-      for(int j =i;j<n;j++)
+      for(int j =i;j<n;j++) // this loop is for finding the minimum element
       {
            if(arr[j]<arr[min_position])
            {
                 min_position = j;
            }
       }
-      swap(arr[i],arr[min_position]);
+      swap(arr[i],arr[min_position]); // then we swap 
    }
    for(int i =0 ; i<n;i++)
    {
