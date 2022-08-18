6eebd532fbc93884476dd21793ad75d0a734e8be
Shreyash Mehrotra <shreyashmehrotra20@gmail.com>
2022-08-18 17:20:09 +0530

    final optimized solution pushed

--- findLcm.cpp
+++ findLcm.cpp
@@ -15,12 +15,31 @@ int lcm(int num1, int num2)
         res++;
     }
 }
+
+
+// optimized solution 
+// IN THIS SOLUTION WE WILL SEE ONE FORMULA THAT IS LCM(A,B) = A*B/GCD(A,B) 
+int gcd(int num1,int num2)
+{
+    if(num2==0)
+    {
+        return num1;
+    }
+    else{
+        return gcd(num2,num1%num2);
+    }
+}
+int optlcm(int num1, int num2)
+{
+      int lcm = (num1*num2)/gcd(num1,num2);
+      return lcm;
+}
 int main()
 {   
     int a , b;
     cout<<"enter numbers for lcm"<<endl;
     cin>>a>>b;
-    int ans = lcm(a,b);
+    int ans = optlcm(a,b);
     cout<<"Answer "<<ans;
     return 0;
 }
