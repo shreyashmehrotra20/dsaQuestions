d984099d7fdc44c2e946626a5bf2fa5b74ffbb3a
Shreyash Mehrotra <shreyashmehrotra20@gmail.com>
2023-02-16 11:58:28 +0530

    pattern

--- lovebabbarQuestions/pattern.cpp
+++ lovebabbarQuestions/pattern.cpp
@@ -2,6 +2,15 @@
 using namespace std;
 
 int main() {
-    cout<<"hello ";
+    
+
+    for(int row =1;row<6;row++)
+    {
+        for(int col=1;col<row+1;col++)
+        {
+            cout<<col;
+        }
+        cout<<endl;
+    }
     return 0; 
 }
