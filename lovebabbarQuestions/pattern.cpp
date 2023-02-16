75e8150b0dd03a20b62d0e64e4cf5ae378c76c83
Shreyash Mehrotra <shreyashmehrotra20@gmail.com>
2023-02-16 12:32:27 +0530

    pattern2 done

--- lovebabbarQuestions/pattern.cpp
+++ lovebabbarQuestions/pattern.cpp
@@ -3,30 +3,53 @@ using namespace std;
 
 int main() {
     int n;
-    cout<<"Enter row";
+    cout<<"Enter row"<<endl;
     cin>>n;
 
-    for(int row =1;row<n;row++)
-    {
+     
+    // pattern 1
+    // for(int row =1;row<n;row++)
+    // {
 
-        if(row ==1 || row ==2 || row == n-1)
-        for(int col=1;col<row+1;col++)
-        {
-            cout<<col;
-        }
-        else {
-            for(int col=1;col<row+1;col++)
+    //     if(row ==1 || row ==2 || row == n-1)
+    //     for(int col=1;col<row+1;col++)
+    //     {
+    //         cout<<col;
+    //     }
+    //     else {
+    //         for(int col=1;col<row+1;col++)
+    //         {
+    //             if(col==1 || col==row)
+    //             {
+    //                 cout<<col<<" ";
+    //             }
+    //             else{
+    //                 cout<<" ";
+    //             }
+    //         }
+    //     }
+    //     cout<<endl;
+    // }
+
+    // pattern 2 
+     for(int row =0;row<n;row++)
+    {   
+        for(int col=row+1;col<n+1;col++)
+        {  
+            if(col==row+1 || col==n || row ==0)
             {
-                if(col==1 || col==row)
-                {
-                    cout<<col;
-                }
-                else{
-                    cout<<" ";
-                }
+                cout<<col;
+            }
+            else{
+                cout<<" ";
             }
         }
         cout<<endl;
     }
+       
+
+        
+    
+
     return 0; 
 }
