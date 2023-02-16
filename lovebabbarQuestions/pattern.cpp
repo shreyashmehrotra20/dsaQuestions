7d815966edc18f76e70cf6906927c392079fa342
Shreyash Mehrotra <shreyashmehrotra20@gmail.com>
2023-02-16 12:08:46 +0530

    pattern q1 done

--- lovebabbarQuestions/pattern.cpp
+++ lovebabbarQuestions/pattern.cpp
@@ -2,13 +2,29 @@
 using namespace std;
 
 int main() {
-    
+    int n;
+    cout<<"Enter row";
+    cin>>n;
 
-    for(int row =1;row<6;row++)
+    for(int row =1;row<n;row++)
     {
+
+        if(row ==1 || row ==2 || row == n-1)
         for(int col=1;col<row+1;col++)
         {
             cout<<col;
+        }
+        else {
+            for(int col=1;col<row+1;col++)
+            {
+                if(col==1 || col==row)
+                {
+                    cout<<col;
+                }
+                else{
+                    cout<<" ";
+                }
+            }
         }
         cout<<endl;
     }
