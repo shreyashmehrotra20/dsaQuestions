7b28717cc0a07a238bcbf563ac5159015e765ad2
Shreyash Mehrotra <shreyashmehrotra20@gmail.com>
2023-02-17 01:27:44 +0530

    pointer

---
+++ lovebabbarQuestions/pointers.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[5] = {5,6,7,8,9}; 
+    // int *n = &arr
+    
+    cout<<&arr<<endl;
+    cout<<&arr+1<<endl;
+    cout<<*(arr+1);
+    return 0; 
+}
