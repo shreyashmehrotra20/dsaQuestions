ef453f063c1ef8ee25830efadf079fbc3606f839
Shreyash Mehrotra <shreyashmehrotra20@gmail.com>
2023-02-17 01:29:20 +0530

    ll

--- lovebabbarQuestions/pointers.cpp
+++ lovebabbarQuestions/pointers.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main() {
     int arr[5] = {5,6,7,8,9}; 
     // int *n = &arr
-    
+    // pointer approch
     cout<<&arr<<endl;
     cout<<&arr+1<<endl;
     cout<<*(arr+1);
