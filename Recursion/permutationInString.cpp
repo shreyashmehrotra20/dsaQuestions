7e86556f038058fe1b88b099d83f5be949d756e2
Shreyash Mehrotra <shreyashmehrotra20@gmail.com>
2023-03-29 02:24:06 +0530

    final code of permutation of string

--- Recursion/permutationInString.cpp
+++ Recursion/permutationInString.cpp
@@ -1,16 +1,30 @@
 #include <iostream>
 using namespace std;
 
-void perString(string &str)
+void perString(string &str,int i)
 {
-    if(i>str.length())
-    {
+    if(i>=str.length())
+    {   
+        cout<<str<<" ";
         return ;
+    }
+
+
+    for(int j=i;j<str.length();j++)
+    {    
+         swap(str[i],str[j]);
+         perString(str,i+1);
+
+         // backtracking  jo changes kiyen hain unko sahi kaardo
+         
+
+         swap(str[i],str[j]);
     }
 }
 int main() {
     
     string str = "abc";
-    
+    int i=0;
+    perString(str,i);
     return 0; 
 }
