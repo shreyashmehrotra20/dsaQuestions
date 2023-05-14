d62f57133c572a3a9b487314bc1a8fbbf69e4096
Shreyash Mehrotra <shreyashmehrotra20@gmail.com>
2023-05-14 10:08:04 +0530

    dynamic array using 2d array

--- OOPs Concepts/dynamicMemory2DArray.cpp
+++ OOPs Concepts/dynamicMemory2DArray.cpp
@@ -1,32 +1,57 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int main() {
     
-    int row = 2;
-    int col = 2;
+    // int row = 2;
+    // int col = 2;
 
-    int** arr = new int*[row];
-    for(int i=0;i<row;i++)
-    {
-        arr[i] = new int [col];
-    }
+    // int** arr = new int*[row];
+    // for(int i=0;i<row;i++)
+    // {
+    //     arr[i] = new int [col];
+    // }
 
-    for(int i=0;i<row;i++)
+    // for(int i=0;i<row;i++)
+    // {
+    //    for(int j=0;j<col;j++)
+    //    {
+    //     arr[i][j] = 0;
+    //    }
+    // }
+
+    // for(int i=0;i<row;i++)
+    // {
+    //    for(int j=0;j<col;j++)
+    //    {
+    //     cout<<arr[i][j]<<" ";
+    //    }
+    //    cout<<endl;
+    // }
+
+    // by using vector same thing 
+
+    vector<vector<int>> arr (3,vector<int>(6,0));
+
+    for(int i=0;i<3;i++)
     {
-       for(int j=0;j<col;j++)
-       {
+      for(int j=0;j<6;j++)
+      {
         arr[i][j] = 0;
-       }
+      }
     }
 
-    for(int i=0;i<row;i++)
+    for(int i=0;i<3;i++)
     {
-       for(int j=0;j<col;j++)
-       {
-        cout<<arr[i][j]<<" ";
-       }
-       cout<<endl;
+      for(int j=0;j<6;j++)
+      {
+       cout<< arr[i][j] <<" ";
+      }
+
+      cout<<endl;
     }
+
+    // vector<vector<int>> (row_size,vector<int> (col_size,intilization))  syntax for dynamic 2d array
     return 0; 
 }
--- OOPs Concepts/tempCodeRunnerFile.cpp
+++ OOPs Concepts/tempCodeRunnerFile.cpp
@@ -1,7 +1,25 @@
-for(int i=0;i<row;i++)
+int row = 2;
+    // int col = 2;
+
+    // int** arr = new int*[row];
+    // for(int i=0;i<row;i++)
     // {
-    //    for(int j=0;i<col;j++)
+    //     arr[i] = new int [col];
+    // }
+
+    // for(int i=0;i<row;i++)
+    // {
+    //    for(int j=0;j<col;j++)
     //    {
     //     arr[i][j] = 0;
     //    }
+    // }
+
+    // for(int i=0;i<row;i++)
+    // {
+    //    for(int j=0;j<col;j++)
+    //    {
+    //     cout<<arr[i][j]<<" ";
+    //    }
+    //    cout<<endl;
     // }
