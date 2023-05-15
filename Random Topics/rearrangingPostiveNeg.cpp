2fa16aa577fbe836f7a4a31077fa48fc6c0f8d9c
Shreyash Mehrotra <shreyashmehrotra20@gmail.com>
2023-05-16 00:12:24 +0530

    done the question very good question

---
+++ Random Topics/rearrangingPostiveNeg.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+//In this code we are arranging positive and negative alternatively without using space O(1)
+
+
+int main() {
+    
+    int arr[6] = {-1,-2,3,-4,-1,5};
+    
+    int i=0;
+    int n = 6;
+    
+    int j=n-1;
+    while(i<j)
+    {
+        while(i<n-1 && arr[i]>0)
+        {
+         i++;
+        }
+        while(j>0 && arr[j]<0){
+            j--;
+        }
+        if(i<j)
+        {
+            swap(arr[i],arr[j]);
+        }
+
+        
+    }
+
+    int k=0;
+    while(k<n && i<n)
+    {
+       if(arr[k]>0 && arr[i]<0)
+       {
+        swap(arr[k],arr[i]);
+        k+=2;
+        i+=1;
+       }
+    }
+
+    //seeing the results
+    
+    for(int i=0;i<n;i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    
+    return 0; 
+}
--- Random Topics/tempCodeRunnerFile.cpp
+++ Random Topics/tempCodeRunnerFile.cpp
@@ -1,1 +1,12 @@
-sum
+ // // while(i<j)
+    // // {
+    // //     while(i<n;)
+    // // }
+
+    // // seeing the results
+
+    // for(int i=0;i<=n;i++)
+    // {
+    //     cout<<arr[i]<<" ";
+    // }
+    
